e287fb5697b7ec6eac65311b27f13faeff3718a9
DESKTOP-1GGU87S\admin <ntngon7@gmail.com>
2026-04-06 15:24:08 +0700

    feat: update CachedTestWebApplicationFactory

--- tests/Demo.Architecture.Test.Shared/Web/CachedTestWebApplicationFactory.cs
+++ tests/Demo.Architecture.Test.Shared/Web/CachedTestWebApplicationFactory.cs
@@ -1,9 +1,12 @@
-﻿using Demo.Architecture.Test.Shared.Services;
+﻿using Demo.Architecture.Infrastructure.Data;
+using Demo.Architecture.Test.Shared.Services;
 using Demo.Architecture.UseCases.Common.Behaviors;
 using Demo.Architecture.UseCases.Common.Interfaces;
 using MediatR;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Data.Sqlite;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection.Extensions;
 using StackExchange.Redis;
@@ -12,23 +15,37 @@ namespace Demo.Architecture.Test.Shared.Web;
 
 public class CachedTestWebApplicationFactory : WebApplicationFactory<Program>
 {
+    private SqliteConnection _connection = null!;
+
     protected override void ConfigureWebHost(IWebHostBuilder builder)
     {
         builder.UseEnvironment("Test");
 
         builder.ConfigureServices(services =>
         {
-            // Remove Redis
+            services.RemoveAll<DbContextOptions<AppDbContext>>();
+            _connection = new SqliteConnection("Data Source=:memory:");
+            _connection.Open();
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlite(_connection);
+            });
+
             services.RemoveAll<ICacheService>();
             services.RemoveAll<IConnectionMultiplexer>();
 
-            // Add MemoryCache
             services.AddMemoryCache();
             services.AddSingleton<ICacheService, MemoryCacheService>();
 
-            // ✅ IMPORTANT: Add CachingBehavior back
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CachingBehavior<,>));
             services.AddTransient(typeof(IPipelineBehavior<,>), typeof(CacheInvalidationBehavior<,>));
         });
+    }
+
+    protected override void Dispose(bool disposing)
+    {
+        base.Dispose(disposing);
+        _connection?.Dispose();
     }
 }
